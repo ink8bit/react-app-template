bf8c173951f85650fe27c1597989ffd21876ec6a
ink8bit <ink8bit@users.noreply.github.com>
2019-09-21 20:59:06 +0300

    Add react-axe
    `react-axe` is a library that audits a React application and logs any
    accessibility issues to the DevTools console.
    
    View more info at https://github.com/dequelabs/react-axe.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 
 import App from './components/App/ClientApp.jsx';
 
-render(React.createElement(App), document.getElementById('root'));
+if (process.env.NODE_ENV !== 'production') {
+  import('react-axe').then(axe => {
+    axe(React, ReactDOM, 1000);
+    render(<App />, document.getElementById('root'));
+  });
+} else {
+  render(<App />, document.getElementById('root'));
+}
