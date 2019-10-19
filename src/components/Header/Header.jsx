08e935e50b84c3aa658d7437a42be05e588a9c24
ink8bit <ink8bit@users.noreply.github.com>
2019-10-19 16:56:50 +0300

    Change Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,51 +1,12 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
 import React, { Component } from 'react';
 import { Link } from '@reach/router';
-import styled from '@emotion/styled';
 import Loadable from 'react-loadable';
 
-import color from '../../styles/color';
-import offset from '../../styles/offset';
 import Modal from './../Modal/Modal.jsx';
-
-const HeaderContainer = styled.header`
-  background-color: ${color.HEADER_BG};
-  padding: 10px ${offset.PADDING_COMMON};
-  height: ${offset.HEADER_HEIGHT};
-  display: flex;
-  justify-content: center;
-
-  li {
-    display: inline-block;
-  }
-
-  li + li {
-    margin-left: 20px;
-  }
-
-  .header__wrapper {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-shrink: 0;
-    width: 100%;
-    max-width: 1000px;
-  }
-`;
-
-const HeaderLogo = styled.img`
-  background-color: ${color.HEADER_LOGO_BG};
-  width: 70px;
-  height: 30px;
-`;
-
-const HeaderLink = styled(Link)`
-  text-decoration: none;
-  color: ${color.LINK};
-
-  &:hover {
-    text-decoration: underline;
-  }
-`;
+import s from './styles.css';
 
 const Loading = () => <span>loading...</span>;
 const LoadableContent = Loadable({
@@ -74,20 +35,24 @@ class Header extends Component {
     ) : null;
 
     return (
-      <HeaderContainer>
-        <div className="header__wrapper">
-          <HeaderLogo onClick={this.toggleModal} src="" alt="logo" title="logo" />
+      <header className={s.header}>
+        <div className={s.headerWrapper}>
+          <img onClick={this.toggleModal} className={s.headerLogo} src="" alt="logo" title="logo" />
           <ul>
-            <li>
-              <HeaderLink to="/">Main</HeaderLink>
+            <li className={s.headerItem}>
+              <Link to="/" className={s.headerLink}>
+                Main
+              </Link>
             </li>
-            <li>
-              <HeaderLink to="/search">Search</HeaderLink>
+            <li className={s.headerItem}>
+              <Link to="/search" className={s.headerLink}>
+                Search
+              </Link>
             </li>
             {ModalLayout}
           </ul>
         </div>
-      </HeaderContainer>
+      </header>
     );
   }
 }
