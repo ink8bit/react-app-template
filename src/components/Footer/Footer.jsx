c857ece241344ba61eed0ec1bd2dafd5c465a95b
ink8bit <ink8bit@users.noreply.github.com>
2019-10-19 16:24:37 +0300

    Change Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,52 +1,29 @@
 import React, { Component } from 'react';
-import { Link } from '@reach/router';
-import styled from '@emotion/styled';
 
-import color from './../../styles/color';
-import offset from './../../styles/offset';
-
-const FooterContainer = styled.footer`
-  background-color: ${color.FOOTER_BG};
-  display: flex;
-  padding: 10px ${offset.PADDING_COMMON};
-  justify-content: center;
-  align-items: center;
-  height: ${offset.FOOTER_HEIGHT};
-
-  li {
-    display: inline-block;
-  }
-
-  li + li {
-    margin-left: 20px;
-  }
-`;
-
-const FooterLink = styled(Link)`
-  text-decoration: none;
-  color: ${color.LINK};
-
-  &:hover {
-    text-decoration: underline;
-  }
-`;
+import s from './style.css';
 
 class Footer extends Component {
   render() {
     return (
-      <FooterContainer className="footer">
-        <ul className="footer__links">
-          <li className="footer__link">
-            <FooterLink to="/">footer link 1</FooterLink>
+      <footer className={s.footer}>
+        <ul>
+          <li className={s.footerItem}>
+            <a className={s.footerLink} href="/">
+              footer link 1
+            </a>
           </li>
-          <li className="footer__link">
-            <FooterLink to="/">footer link 2</FooterLink>
+          <li className={s.footerItem}>
+            <a className={s.footerLink} href="/">
+              footer link 2
+            </a>
           </li>
-          <li className="footer__link">
-            <FooterLink to="/">footer link 3</FooterLink>
+          <li className={s.footerItem}>
+            <a className={s.footerLink} href="/">
+              footer link 3
+            </a>
           </li>
         </ul>
-      </FooterContainer>
+      </footer>
     );
   }
 }
