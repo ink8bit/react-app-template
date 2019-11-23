1c6ae9ebc5101f11ee9bb2ef0fc3b49d7656f181
ink8bit <ink8bit@users.noreply.github.com>
2019-11-23 20:19:57 +0300

    Fix ModalContent test

--- src/components/Modal/__tests__/ModalContent.test.js
+++ src/components/Modal/__tests__/ModalContent.test.js
@@ -4,6 +4,6 @@ import { create } from 'react-test-renderer';
 import ModalContent from './../ModalContent.jsx';
 
 test('snapshot', () => {
-  const c = create(<ModalContent toggleModal />);
+  const c = create(<ModalContent setModalVisible={() => {}} />);
   expect(c.toJSON()).toMatchSnapshot();
 });
