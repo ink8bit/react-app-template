1f17719fa37c9569788f516d2837ddadce591faf
ink8bit <ink8bit@users.noreply.github.com>
2019-11-23 14:33:44 +0300

    Change Image markup

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -4,6 +4,27 @@ import PropTypes from 'prop-types';
 import Image from './../Image/Image.jsx';
 import s from './styles.css';
 
+const images = [
+  {
+    src:
+      'https://images.unsplash.com/photo-1549558549-415fe4c37b60?ixlib=rb-1.2.1&auto=format&fit=crop&w=2765&q=80',
+    websiteLink: 'https://unsplash.com/@mar28mar',
+    author: 'Mar Bustos'
+  },
+  {
+    src:
+      'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80',
+    websiteLink: 'https://unsplash.com/@johnonolan',
+    author: "John O'Nolan"
+  },
+  {
+    src:
+      'https://images.unsplash.com/photo-1516898263-bc9e1a4e7e8d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80',
+    websiteLink: 'https://unsplash.com/@callumstewartni',
+    author: 'Callum Stewart'
+  }
+];
+
 class Content extends Component {
   constructor(props) {
     super(props);
@@ -12,34 +33,27 @@ class Content extends Component {
   }
 
   render() {
+    const imageList = images.map(image => {
+      const { src, websiteLink, author } = image;
+      return (
+        <li key={src} className={s.image}>
+          <Image src={src} websiteLink={websiteLink} author={author} />
+        </li>
+      );
+    });
+
     return (
       <main className={s.content}>
-        <h1 className={s.contentTitle}>{this.props.message}</h1>
+        <h1 className={s.title}>{this.props.message}</h1>
 
         <div>
           <p>Open network panel</p>
           <p>Scroll down</p>
           <p>You will see how images are loading</p>
-          <div className={s.contentBuffer} />
+          <div className={s.buffer} />
         </div>
 
-        <ul>
-          <Image
-            src="https://images.unsplash.com/photo-1549558549-415fe4c37b60?ixlib=rb-1.2.1&auto=format&fit=crop&w=2765&q=80"
-            websiteLink="https://unsplash.com/@mar28mar"
-            author="Mar Bustos"
-          />
-          <Image
-            src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80"
-            websiteLink="https://unsplash.com/@johnonolan"
-            author="John O'Nolan"
-          />
-          <Image
-            src="https://images.unsplash.com/photo-1516898263-bc9e1a4e7e8d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80"
-            websiteLink="https://unsplash.com/@callumstewartni"
-            author="Callum Stewart"
-          />
-        </ul>
+        <ul>{imageList}</ul>
       </main>
     );
   }
--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 import s from './styles.css';
 
-class Image extends Component {
-  render() {
-    return (
-      <li className={s.imageContainer}>
-        <img className={classNames(s.image, 'lazy')} src="" data-src={this.props.src} alt="" />
+function Image(props) {
+  const { src, author, websiteLink } = props;
 
-        <div className={s.imageInfo}>
-          <p className={s.imageAuthor}>{this.props.author}</p>
-          <a
-            href={this.props.websiteLink}
-            title="unsplash.com link"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={s.imageLink}
-          >
-            Unsplash link
-          </a>
-        </div>
-      </li>
-    );
-  }
+  return (
+    <>
+      <img className={classNames(s.image, 'lazy')} src="" data-src={src} alt="" />
+      <div className={s.imageInfo}>
+        <p className={s.imageAuthor}>{author}</p>
+        <a
+          href={websiteLink}
+          title="unsplash.com link"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={s.imageLink}
+        >
+          Unsplash link
+        </a>
+      </div>
+    </>
+  );
 }
 
 Image.defaultPropTypes = {
