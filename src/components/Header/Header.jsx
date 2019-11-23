92cc744a59bb2d834bbd6c21928a9430764ea960
ink8bit <ink8bit@users.noreply.github.com>
2019-11-23 20:18:56 +0300

    Change Modal implementation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,57 +1,31 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 
-import React, { Component, Suspense } from 'react';
+import React from 'react';
 import { Link } from '@reach/router';
 
-import Modal from './../Modal/Modal.jsx';
 import s from './styles.css';
 
-const LoadableContent = React.lazy(() => import('./../Modal/ModalContent.jsx'));
-
-class Header extends Component {
-  state = {
-    showModal: false
-  };
-
-  toggleModal = () => {
-    this.setState({
-      showModal: !this.state.showModal
-    });
-  };
-
-  render() {
-    const { showModal } = this.state;
-
-    const ModalLayout = showModal ? (
-      <Modal>
-        <Suspense fallback={<div>Loading...</div>}>
-          <LoadableContent toggleModal={this.toggleModal} name={name} />
-        </Suspense>
-      </Modal>
-    ) : null;
-
-    return (
-      <header className={s.header}>
-        <div className={s.headerWrapper}>
-          <img onClick={this.toggleModal} className={s.headerLogo} src="" alt="logo" title="logo" />
-          <ul>
-            <li className={s.headerItem}>
-              <Link to="/" className={s.headerLink}>
-                Main
-              </Link>
-            </li>
-            <li className={s.headerItem}>
-              <Link to="/search" className={s.headerLink}>
-                Search
-              </Link>
-            </li>
-            {ModalLayout}
-          </ul>
-        </div>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header className={s.header}>
+      <div className={s.wrapper}>
+        <img className={s.logo} src="" alt="logo" title="logo" />
+        <ul>
+          <li className={s.item}>
+            <Link to="/" className={s.link}>
+              Main
+            </Link>
+          </li>
+          <li className={s.item}>
+            <Link to="/search" className={s.link}>
+              Search
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
