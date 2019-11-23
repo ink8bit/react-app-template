06914dc01cffd6dc30eaddff85cded35599f0316
ink8bit <ink8bit@users.noreply.github.com>
2019-11-23 18:58:28 +0300

    Change App markup and styles

--- src/components/App/ClientApp.jsx
+++ src/components/App/ClientApp.jsx
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Router } from '@reach/router';
 
 import './styles.css';
-
 import Header from './../../components/Header/Header.jsx';
 import Content from './../Content/Content.jsx';
 import Footer from './../Footer/Footer.jsx';
 import './../../utils/lazy-loading';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <Router>
-          <Content path="/" message="main" />
-          <Content path="/search" message="search" />
-        </Router>
-        <Footer />
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Router>
+        <Content path="/" message="main" />
+        <Content path="/search" message="search" />
+      </Router>
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default App;
