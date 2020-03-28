6094d0bf51c1a6c510fc0b8c269bd02f4d0a36f2
ink8bit <ink8bit@users.noreply.github.com>
2020-03-28 21:02:03 +0300

    Remove useless lint comments

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-
 import React from 'react';
 import { Link } from '@reach/router';
 
